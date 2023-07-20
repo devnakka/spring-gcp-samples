8d04a996ffa10a52334310f1c4144ea473f628ce
Srikanth Nakka <srikanth.nakka@exabeam.com>
2023-07-20 12:08:21 +0530

    initial commit

---
+++ src/main/java/com/devnakka/springgcpsamples/SpringGcpSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.devnakka.springgcpsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGcpSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGcpSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devnakka/springgcpsamples/SpringGcpSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnakka.springgcpsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGcpSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
